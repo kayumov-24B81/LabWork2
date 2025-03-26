278091df9f031d66d2ace4e246e760a839f3f492
Airat Kayumov <st128100@student.spbu.ru>
2025-03-27 01:09:09 +0300

    Added Weapon class. Weapon is the parent class for future weapons classes. One was leaved undefined due to absence of Player class definition.

---
+++ include/weapon.hpp
@@ -0,0 +1,24 @@
+#ifndef WEAPON_HPP
+#define WEAPON_HPP
+
+#include "effect.hpp"
+
+class Weapon
+{
+    private:
+        std :: string name;
+        int damage;
+        int defense;
+    public:
+        Weapon(std :: string name, int dmg, int def);
+        virtual ~Weapon() = 0;
+        std :: string getName();
+        int getDamage();
+        int getDefense();
+        virtual void atkEffect(Player* owner, Player* target) = 0;
+        virtual void defEffect(Player* owner) = 0;
+        virtual void empEffect(Player* owner, Player* target) = 0;
+        void revertStats(Player* owner);
+};
+
+#endif
---
+++ src/weapon.cpp
@@ -0,0 +1,27 @@
+#include "weapon.hpp"
+
+Weapon :: Weapon(std :: string name, int dmg, int def):
+    name(name), damage(dmg), defense(def)
+{
+}
+
+std :: string Weapon ::  getName()
+{
+    return name;
+}
+
+int Weapon ::  getDamage()
+{
+    return damage;
+}
+
+int Weapon :: getDefense()
+{
+    return defense;
+}
+
+void revertStats(Player* owner)
+{
+    /*to be done*/
+}
+
